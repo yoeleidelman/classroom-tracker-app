0f308ea2bdf5e1edbef83d2ebf2b410856bc6236
Yoel Eidelman <yoeleidel@gmail.com>
2026-08-02 10:19:31 -0700

    Use createRequire for firebase-admin

--- api/create-teacher.js
+++ api/create-teacher.js
@@ -1,5 +1,6 @@
-import pkg from "firebase-admin";
-   const admin = pkg.apps ? pkg : pkg.default;
+import { createRequire } from "module";
+   const require = createRequire(import.meta.url);
+   const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
   admin.initializeApp({
